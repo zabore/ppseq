6d22a9e47091c9bfdb71b663aab654b876efe9b4
Emily C. Zabor <emily.zabor@gmail.com>
2021-03-31 10:20:21 -0400

    update calibrate_pp_ppp_threshold and vignette

--- R/globals.R
+++ R/globals.R
@@ -1,1 +1,4 @@
-utils::globalVariables(c("n0", "n1", "positive", "pp_threshold"))
+utils::globalVariables(c("n0", "n1", "positive", "pp_threshold",
+                         "ppp", "y0", "y1", "n0_null", "n1_null",
+                         "positive_null", "n0_alt", "n1_alt",
+                         "positive_alt"))
