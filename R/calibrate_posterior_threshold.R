f758f8d76cfe82eead208d9373135a745f322f2d
Emily C. Zabor <emily.zabor@gmail.com>
2021-09-03 13:45:13 -0400

    fixing a couple of typos

--- R/calibrate_posterior_threshold.R
+++ R/calibrate_posterior_threshold.R
@@ -31,7 +31,7 @@
 #' associated proportion of positive trials.
 #'
 #' @examples
-#'
+#' \dontrun{
 #' set.seed(123)
 #'
 #' # One-sample case
@@ -40,13 +40,12 @@
 #'   p0 = 0.1, delta = NULL, prior = c(0.5, 0.5), S = 5000, theta = c(0.9, 0.95)
 #' )
 #'
-#' \dontrun{
 #' # Two-sample case
 #' calibrate_posterior_threshold(
 #' p = c(0.1, 0.1), N = c(50, 50),
 #' direction = "greater", p0 = NULL, delta = 0, prior = c(0.5, 0.5),
 #' S = 5000, theta = c(0.9, 0.95)
-#' # )
+#' )
 #' }
 #' @importFrom purrr map_dbl map2_dbl
 #' @importFrom stats rbinom
