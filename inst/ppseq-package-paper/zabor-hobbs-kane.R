ab09f0e5c7292d0c646e7539daa62e1ea0f86d2c
Emily C. Zabor <emily.zabor@gmail.com>
2022-01-24 09:43:50 -0500

    close to final paper draft for submission

--- inst/ppseq-package-paper/zabor-hobbs-kane.R
+++ inst/ppseq-package-paper/zabor-hobbs-kane.R
@@ -71,6 +71,138 @@ p <- plot(
 p
 
 
+## ----echo = FALSE, eval = FALSE-----------------------------------------------
+#> library(dplyr)
+#> x <- one_sample_cal_tbl
+#> type1_range <- c(0.05, 0.1)
+#> minimum_power <- 0.7
+#> 
+#> plot_x <-
+#>   rename(
+#>     mutate(
+#>       filter(
+#>         x$res_summary,
+#>         prop_pos_null >= type1_range[1] &
+#>           prop_pos_null <= type1_range[2] &
+#>           prop_pos_alt >= minimum_power
+#>       ),
+#>       Design = paste0(
+#>         "Posterior threshold = ", pp_threshold,
+#>         " and predictive threshold = ", ppp_threshold
+#>       ),
+#>       ab_dist_metric = ((prop_pos_null - 0)^2 +
+#>         (prop_pos_alt - 1)^2)^(1 / 2),
+#>       n_dist_metric = ((mean_n1_null - min(mean_n1_null))^2 +
+#>         (mean_n1_alt - max(mean_n1_alt))^2)^(1 / 2)
+#>     ),
+#>     `Type I error` = prop_pos_null,
+#>     Power = prop_pos_alt,
+#>     `Average N under the null` = mean_n1_null,
+#>     `Average N under the alternative` = mean_n1_alt,
+#>     `Distance to optimal efficiency` = n_dist_metric,
+#>     `Distance to optimal accuracy` = ab_dist_metric
+#>   )
+#> 
+#> 
+#> plot_ab <-
+#> mutate(
+#>   ungroup(
+#>     slice(
+#>       group_by(
+#>         arrange(
+#>           plot_x,
+#>           `Distance to optimal accuracy`,
+#>           -pp_threshold,
+#>           -ppp_threshold
+#>         ),
+#>         `Distance to optimal accuracy`
+#>       ),
+#>       1
+#>     )
+#>   ),
+#>   optimal_accuracy = ifelse(row_number() == 1, TRUE, FALSE)
+#> )
+#> 
+#> plot_nn <-
+#> mutate(
+#>   ungroup(
+#>     slice(
+#>       group_by(
+#>         arrange(
+#>           plot_x,
+#>           `Distance to optimal efficiency`,
+#>           -pp_threshold, -ppp_threshold
+#>         ),
+#>         `Distance to optimal efficiency`
+#>       ),
+#>       1
+#>     )
+#>   ),
+#>   optimal_efficiency = ifelse(row_number() == 1, TRUE, FALSE)
+#> )
+#> 
+#> p1 <-
+#> ggplot(
+#>   plot_ab,
+#>   aes(
+#>     x = `Type I error`,
+#>     y = Power,
+#>     color = `Distance to optimal accuracy`,
+#>     Design = Design,
+#>     `Average N under the null` = `Average N under the null`,
+#>     `Average N under the alternative` = `Average N under the alternative`
+#>   )
+#> ) +
+#> geom_point(
+#>   shape = ifelse(plot_ab$optimal_accuracy == TRUE, 18, 19),
+#>   size = ifelse(plot_ab$optimal_accuracy == TRUE, 4, 2)
+#> ) +
+#> ylim(0, 1) +
+#> xlim(0, 1) +
+#> labs(
+#>   x = "Type I error",
+#>   y = "Power"
+#> ) +
+#> scale_color_viridis_c() +
+#> theme_bw() +
+#> theme(legend.position = "bottom")
+#> 
+#> p2 <-
+#> ggplot(
+#>   plot_nn,
+#>   aes(
+#>     x = `Average N under the null`,
+#>     y = `Average N under the alternative`,
+#>     color = `Distance to optimal efficiency`,
+#>     Design = Design,
+#>     `Type I error` = `Type I error`,
+#>     Power = Power
+#>   )
+#> ) +
+#> geom_point(
+#>   shape = ifelse(plot_nn$optimal_efficiency == TRUE, 18, 19),
+#>   size = ifelse(plot_nn$optimal_efficiency == TRUE, 4, 2)
+#> ) +
+#> ylim(
+#>   min(plot_x$`Average N under the null`),
+#>   max(plot_x$`Average N under the alternative`)
+#> ) +
+#> xlim(
+#>   min(plot_x$`Average N under the null`),
+#>   max(plot_x$`Average N under the alternative`)
+#> ) +
+#> labs(
+#>   x = "Average N under the null",
+#>   y = "Average N under the alternative"
+#> ) +
+#> scale_color_viridis_c() +
+#> theme_bw() +
+#> theme(legend.position = "bottom")
+#> 
+#> 
+#>   p1 + p2
+
+
 ## ----echo = FALSE, fig.width = 6, fig.height = 4, include=knitr::is_html_output(), eval=knitr::is_html_output(), fig.cap = "Plot of design options made with plotly. The color represents the Euclidean distance to the top left point and the optimal design is indicated by a diamond."----
 #> p <- plot(
 #>   one_sample_cal_tbl,
