35cb1d743083e5c7cbe0c87f1de0ea540fe65b62
Emily C. Zabor <emily.zabor@gmail.com>
2024-01-22 15:36:20 -0500

    try to add plot tests back in with new snapshots

--- tests/testthat/test-calc_decision_rules.R
+++ tests/testthat/test-calc_decision_rules.R
@@ -24,7 +24,7 @@ test_that(
   "two-sample calc_decision_rules works",
   {
     set.seed(123)
-    skip_on_ci() # I don't know why but this test keeps failing on GitHub Actions testing
+    # skip_on_ci() # I don't know why but this test keeps failing on GitHub Actions testing
     expect_snapshot_output(
       calc_decision_rules(
         n = cbind(c(10, 20), c(10, 20)), 
